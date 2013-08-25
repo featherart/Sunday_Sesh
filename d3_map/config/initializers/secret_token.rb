50e8fda8ebf7d00983873461beb40c48b42be3dd
Feather <lisafeather@gmail.com>
2013-08-25 14:51:30 -0400

    add 2nd file

---
+++ d3_map/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D3Map::Application.config.secret_key_base = '414c3a437733b1634a0581668807a724a7fe314e5ffc7dd0008cffea3dec4415e489f05daaaa329a562161dbd9df6c9c564cc3509dcfac15d2323ed4874181e7'
